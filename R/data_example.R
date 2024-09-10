3850276d8616ed4de8ba2e8470dc03dc3cddd244
AlbertSL98 <152312456+AlbertSL98@users.noreply.github.com>
2024-09-10 09:46:36 -0400

    change

---
+++ R/data_example.R
@@ -0,0 +1,22 @@
+#' Sample otu table: otu.tab
+#'
+#' A sample otu table for demonstration purposes.
+#'
+#' @format A Matrix with 30 rows and 20 columns:
+#' \describe{
+#'   \item{rows}{Each row is a sample.}
+#'   \item{columns}{Each column is the abundance of Taxon.}
+#' }
+#' @source Simulated data
+"otu.tab"
+
+#' Sample meta data: metadata
+#'
+#' Meta data for demonstration purposes.
+#'
+#' @format A data frame with A rows and B columns:
+#' \describe{
+#'   \item{Meta data}{This is the simulated meta data for demonstration purposes, which includes Abx, age and study.}
+#' }
+#' @source Simulated data
+"metadata"
